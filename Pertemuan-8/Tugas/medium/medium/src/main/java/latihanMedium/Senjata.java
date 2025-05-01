2253feef98e1026e033970e0ae43cb6f4b57d317
thegamingroyal <100133994+thegamingroyal@users.noreply.github.com>
2025-05-01 22:20:20 +0700

    Add files via upload

---
+++ Pertemuan-8/Tugas/medium/medium/src/main/java/latihanMedium/Senjata.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanMedium;
+
+/**
+ *
+ * @author adity
+ */
+public class Senjata {
+    private String bunyi;
+    private boolean menusuk;
+    private int peluru;
+
+    public Senjata(String bunyi) {
+        this.bunyi = bunyi;
+        this.menusuk = false;
+        this.peluru = 0;
+    }
+
+    public String getBunyi() {
+        return bunyi;
+    }
+
+    public int getPeluru() {
+        return peluru;
+    }
+
+    public boolean isMenusuk() {
+        return menusuk;
+    }
+
+    public void setBunyi(String bunyi) {
+        this.bunyi = bunyi;
+    }
+
+    public void setMenusuk(boolean menusuk) {
+        this.menusuk = menusuk;
+    }
+
+    public void setPeluru(int peluru) {
+        this.peluru = peluru;
+    }
+
+    public void pasangBayonet() {
+        setMenusuk(true);
+    }
+
+    public String menusuk() {
+        if (isMenusuk()) {
+            return "Jleb!";
+        } else {
+            return "Gagal, belum pasang bayonet";
+        }
+    }
+}
---
+++ Pertemuan-8/Tugas/medium/medium/src/main/java/latihanMedium/TestSenjata.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanMedium;
+
+/**
+ *
+ * @author adity
+ */
+public class TestSenjata {
+
+    public static void main(String[] args) {
+        Senjata ak47 = new Senjata("TAR");
+        Senjata m16 = new Senjata("DOR");
+
+        KontrolSenjata kontrolAK47 = new KontrolSenjata(ak47);
+        KontrolSenjata kontrolM16 = new KontrolSenjata(m16);
+
+        kontrolAK47.menembak(2);
+        kontrolAK47.isiPeluru(3);
+        kontrolAK47.menembak(5);
+
+        System.out.println("================================");
+
+        kontrolM16.menembak(1);
+        kontrolM16.isiPeluru(5);
+        kontrolM16.menembak(2);
+    }
+}
