ca7e843434e41b5a8e1bd186251c0a72a98b1a73
thegamingroyal <100133994+thegamingroyal@users.noreply.github.com>
2025-05-01 22:31:02 +0700

    Add files via upload

---
+++ Pertemuan-8/Tugas/medium/medium/src/main/java/latihanMedium/KontrolSenjata.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanMedium;
+
+/**
+ * File      : KontrolSenjata.java
+ * Deskripsi : Kelas kontrol untuk mengelola operasi menembak dan isi peluru pada objek Senjata.
+ * Pembuat   : Aditya Arif Setiawan
+ * Tanggal   : 1 Mei 2025
+ */
+public class KontrolSenjata {
+    private Senjata senjata;
+
+    public KontrolSenjata(Senjata s) {
+        this.senjata = s;
+    }
+
+    public boolean isAdaPeluru() {
+        return senjata.getPeluru() > 0;
+    }
+
+    public void isiPeluru(int jumlahPeluru) {
+        int peluruAwal = senjata.getPeluru();
+        senjata.setPeluru(peluruAwal + jumlahPeluru);
+        System.out.println(">> Peluru berhasil ditambah: " + jumlahPeluru);
+    }
+
+    public void menembak(int jumlah) {
+        System.out.println(">> Siap menembak " + jumlah + " kali");
+        if (!isAdaPeluru()) {
+            System.out.println("Peluru Habis");
+            return;
+        }
+
+        for (int i = 0; i < jumlah; i++) {
+            if (senjata.getPeluru() > 0) {
+                System.out.println(senjata.getBunyi());
+                senjata.setPeluru(senjata.getPeluru() - 1);
+            } else {
+                System.out.println("Gagal tembak, Peluru Habis");
+            }
+        }
+
+        System.out.println(">> Peluru sisa: " + senjata.getPeluru());
+    }
+}
